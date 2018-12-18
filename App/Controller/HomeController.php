6605dd92a87c23e2668f730eda4e3251899b2b98
SandratraRM <sandratrarambelosonm@gmail.com>
2018-12-18 23:36:28 +0300

    Controller

---
+++ App/Controller/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use Mora\Core\Control\Controller;
+
+class HomeController extends Controller{
+    public function index($params){
+        
+    }
+    public function ActionNotFound($params){
+        
+    }
+}
